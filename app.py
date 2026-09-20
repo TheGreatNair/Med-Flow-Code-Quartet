10a0f32e138732952736d139107ec6cca257d0a4
Clown2303 <166534150+Clown2303@users.noreply.github.com>
2026-09-20 13:26:55 +0530

    UI change

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 import streamlit as st
+st.set_page_config(
+    page_title="MEDFLOW Hospital Triage",
+    page_icon="🏥",
+    layout="wide"
+)
 import time
 from Patients import PatientPriorityQueue, Patient, Urgency
 from Resources import SurgeryEquipmentHandler
